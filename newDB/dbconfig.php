1957b38769fb470a97e9e9193a22be395ba4ab97
엄지 <70639677+eooomji@users.noreply.github.com>
2021-11-21 18:22:48 +0900

    새 DB 만들고 테이블과 더미값들은 db.sql 복붙

---
+++ newDB/dbconfig.php
@@ -0,0 +1,19 @@
+<?php
+// 에러처리
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+// json 통신
+header("Content-Type:application/json");
+
+$host = '127.0.0.1';
+$user = 'root';
+$pw = 'Ejms920313';
+$dbName = 'webproj';
+
+$db = new mysqli($host, $user, $pw, $dbName);
+
+mysqli_set_charset($db, 'utf8'); 
+
+mysqli_close($db);
+?>

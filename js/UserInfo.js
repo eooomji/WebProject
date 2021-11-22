2282412210c07683104c00320b7cf95ce0e1d819
tmnt867 <jyj199803@gmail.com>
2021-11-22 23:24:18 +0900

    211122

--- js/UserInfo.js
+++ js/UserInfo.js
@@ -1,26 +1,11 @@
-import { SessionCheck } from './module.js';
-
-/*/
-const SessionCheck = () => {
-	fetch('../php/sessionCheck.php', { method: 'GET', headers: { 'Content-Type': 'application/json' } }).then(Response => {
-		CheckHttpResponseStatus(Response);
-	}).then(Result => {
-		if (Result === false) {
-			location.replace("../html/login.html");
-		}
-	}).catch(error => {
-		alert(error);
-	});
-}
-/*/
-
-const CheckHttpResponseStatus = (res => {
+﻿
+const CheckHttpResponseStatus = res => {
 	if (res.statusText === "OK") {
 		return res.json();
 	} else {
 		throw `${res.status} ${res.statusText}`;
 	}
-})
+}
 
 onload = () => {
 	SessionCheck();
@@ -29,83 +14,97 @@ onload = () => {
 }
 
 const LoadUser = () => {
-	document.getElementsByName(`SettingItem`).forEach((element) => {
-		element.checked = true;
-		element.checked = false;
-	});
 	fetch('../php/LoadUser.php', { method: 'GET', headers: { 'Content-Type': 'application/json' } }).then(Response => {
 		return CheckHttpResponseStatus(Response);
 	}).then(Result => {
 		Object.keys(Result).forEach(element => {
 			try {
-				const target = document.getElementById(element);
-				if (target.checked != null) {
-					target.checked = Result[element];
-				} else if (target.value != null) {
-					target.value = Result[element];
-				} else if (target.textContent != null) {
-					target.textContent = Result[element];
+				let property;
+				switch (true) {
+					case element.includes('Setting'):
+						property = 'checked';
+						break;
+					case element.includes('score'):
+						property = 'textContent';
+						break;
+					default:
+						property = 'value';
 				}
+				document.getElementsByName(element)[0][property] = Result[element];
 			} catch (error) {
 				switch (element) {
 					case 'IsOAuth':
 						if (Result[element] == true) {
 							document.querySelectorAll('.HideOnOAuth').forEach(element => {
-								element.style.display = 'None';
+								element.remove();
 							});
 						}
 						break;
+					case 'choice':
+						document.getElementsByName('SettingItem').forEach((element, index) => {
+							element.checked = (Result['choice'] & Math.pow(2, index)) != 0 ? true : false;
+						});
+						break;
 					default:
-						alert(`${error} ${element}`);
+						throw `${error} ${element}`;
 				}
 			}
 		});
 	}).catch(error => {
 		alert(error);
-	});
+	})
 }
 
+const GetChoiceValue = () => {
+	let choice = 0;
+	document.getElementsByName('SettingItem').forEach((element, index) => {
+		choice += element.checked == true ? Math.pow(2, index) : 0;
+	});
+	return choice;
+}
 
 const DisableUserInfoChange = (IsDisable) => {
-	document.getElementsByName(`UserInfoItem`).forEach((element) => {
+	document.querySelectorAll('.UserInfoItem').forEach(element => {
 		element.disabled = IsDisable;
 	});
 	document.getElementById('changeinfo').style.visibility = IsDisable ? 'hidden' : 'visible';
 }
 
 const onClickChangeUserInfo = () => {
-	SaveChange('UserInfo', 'value', (Result) => {
-		Result["Result"] === "Success" ? DisableUserInfoChange(true) : null;
-		ShowWaiterForResponse('UserInfo', Result, false);
+	if (document.getElementById('UserPassword').value !== document.getElementById('UserPasswordCheck').value) {
+		document.querySelector('#UserInfoResult').textContent = `sd`;
+		return;
+	}
+	SaveChange('UserInfo', GetData('UserInfo', 'value'), (Result) => {
+		if (Result["Result"] === "Success") {
+			DisableUserInfoChange(true);
+		}
 	});
 }
 
 const onClickChangeSetting = () => {
-	SaveChange('Setting', GetData('Setting', 'checked'), (Result) => {
-		ShowWaiterForResponse('Setting', Result, false);
+	SaveChange('Setting', { choice: GetChoiceValue() }, (Result) => {
 	});
 }
 
 const SaveChange = (target, Data, ProcessFunc) => {
-	ShowWaiterForResponse(target, true, true);
+	document.getElementById(`${target}LoadingCircle`).style.visibility = 'visible';
 	fetch(`../php/Change${target}.php`, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(Data) }).then(Response => {
 		return CheckHttpResponseStatus(Response);
 	}).then(Result => {
+		document.getElementById(`${target}Result`).textContent = Result["Message"];
 		ProcessFunc(Result);
 	}).catch(error => {
 		alert(error);
+	}).finally(() => {
+		document.getElementById(`${target}LoadingCircle`).style.visibility = 'hidden';
 	});
-}
-
-const ShowWaiterForResponse = (target, Result, IsStart) => {
-	document.getElementById(`${target}LoadingCircle`).style.visibility = IsStart ? 'visible' : 'hidden';
-	document.getElementById(`${target}Result`).textContent = IsStart ? "" : Result["Message"];
 }
 
 const GetData = (target, property) => {
 	const Data = {};
-	document.getElementsByName(`${target}Item`).forEach((element) => {
-		Data[element.id] = element[property];
+	document.querySelectorAll(`.${target}Item`).forEach((element) => {
+		Data[element.name] = element[property];
 	});
 	return Data;
 }
--- php/ChangeUserInfo.php
+++ php/ChangeUserInfo.php
@@ -3,23 +3,23 @@ require_once("dbconfig.php");
 
 $_POST = JSON_DECODE(file_get_contents("php://input"), true);
 
-$UserName = $_POST["UserName"];
-$password = password_hash($_POST["UserPassword"], PASSWORD_BCRYPT);
-$UserNickName = $_POST["UserNickName"]; 
-$UserEmail = $_POST["UserEmail"];
+$name= $_POST["name"];
+$password = password_hash($_POST["password"], PASSWORD_BCRYPT);
+$username = $_POST["username"]; 
+$email = $_POST["email"];
 
 session_start();
 $Name_on_Session = $_SESSION["sess_username"];
 
 
-$sql = "UPDATE `test`.`user` SET `UserName` = '$UserName', `password` = '$password', `name` = '$UserNickName', `email` = '$UserEmail' WHERE `UserName` = '$Name_on_Session'";
+$sql = "UPDATE `test`.`user` SET `username` = '$email', `password` = '$password', `name` = '$name', `email` = '$email' WHERE `username` = '$Name_on_Session'";
 $res = $db->query($sql); 
 
 $success = $res;
 
 if($success) {
-	if($_SESSION["sess_username"] != $UserName) {
-		$_SESSION["sess_username"] = $UserName;
+	if($_SESSION["sess_username"] != $username) {
+		$_SESSION["sess_username"] = $username;
 	}
 	$result = array("Result"=>"Success", "Message"=>"Success");
 } else {

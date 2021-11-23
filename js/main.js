059f4f7118478fe4519e218a8d7b8d60867dc191
최승호 <75907683+etaF-LODi@users.noreply.github.com>
2021-11-23 23:56:13 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,5 +13,5 @@ window.onload = async function() {
   const getMission = await axios.post('../php/getMissions.php', {userName: userName, nowMonth: nowMonth+1});
   const getMonth = await axios.post('../php/getMissionDay.php', {userName: userName, nowMonth: nowMonth+1});;
 
-  document.querySelector('.mission').innerText += ${getMission.data[getMission[getMonth.data.length].missionName]};
+  document.querySelector('.mission').innerText = '${getMission.data[getMission[getMonth.data.length].missionName]}';
 }

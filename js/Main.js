d49bed4368692a389b7feafa5b8d383aa0f042a8
최승호 <75907683+etaF-LODi@users.noreply.github.com>
2021-11-23 23:08:01 +0900

    Update Main.js

--- js/Main.js
+++ js/Main.js
@@ -1,78 +1,8 @@
 let ToDay = new Date(); 
 let nowMonth = ToDay.getMonth(); 
 
-const CheckHttpResponseStatus = res => {
-	if (res.statusText === "OK") {
-		return res.json();
-	} else {
-		throw `${res.status} ${res.statusText}`;
-	}
-}
-
-onload = () => {
-	SessionCheck();
-	LoadInfo();
-	DisableUserInfoChange(true);
-}
-
-//Calander와 UserInfo 통합.
-const LoadInfo = () => {
-	fetch('../php/LoadUser.php', { method: 'GET', headers: { 'Content-Type': 'application/json' } }).then(Response => {
-		return CheckHttpResponseStatus(Response);
-	}).then(Result => {
-		Object.keys(Result).forEach(element => {
-			try {
-				let property;
-				switch (true) {
-					case element.includes('Setting'):
-						property = 'checked';
-						break;
-					case element.includes('score'):
-						property = 'textContent';
-						break;
-					default:
-						property = 'value';
-				}
-				document.getElementsByName(element)[0][property] = Result[element];
-			} catch (error) {
-				switch (element) {
-					case 'IsOAuth':
-						if (Result[element] == true) {
-							document.querySelectorAll('.HideOnOAuth').forEach(element => {
-								element.remove();
-							});
-						}
-						break;
-					case 'choice':
-						document.getElementsByName('SettingItem').forEach((element, index) => {
-							element.checked = (Result['choice'] & Math.pow(2, index)) != 0 ? true : false;
-						});
-						break;
-					default:
-						throw `${error} ${element}`;
-				}
-			}
-		});
-	}).catch(error => {
-		alert(error);
-	})
-
-    SessionCheck();
-
-    const dateInArr = ['dateIn1', 'dateIn2', 'dateIn3', 'dateIn4', 'dateIn5', 'dateIn6', 'dateIn7', 'dateIn8', 'dateIn9', 'dateIn10', 'dateIn11', 'dateIn12', 'dateIn13', 'dateIn14', 'dateIn15', 'dateIn16', 'dateIn17', 'dateIn18', 'dateIn19', 'dateIn20', 'dateIn21', 'dateIn22', 'dateIn23', 'dateIn24', 'dateIn25', 'dateIn26', 'dateIn27', 'dateIn28', 'dateIn29', 'dateIn30', 'dateIn31'];
-    const getName = await axios.get('../php/LoadUser.php');
-    let userName = getName.data.username;
-    const monthDay2 = await axios.post('../php/getMissionDay.php', {userName: userName, nowMonth: nowMonth+1});
-
-    let mission = document.querySelector(`.missions${monthDay2.data.length}`);
-    mission.style.display='none';
-    
-    const mission = document.querySelector(`.missions${monthDay2.data.length+1}`);
-    mission.style.display='';  
-}
-
-// 미션 위젯에서 오늘 미션 보여주기
-const arr = async(nowMonth) => {
+// DB에서 미션 다 불러오기
+const arr = async(nowMonth, userName) => {
   try {
     let missionHTML = '';
     const response = await axios.post('../php/getMissions.php', {userName: userName, nowMonth: nowMonth+1});
@@ -116,11 +46,47 @@ const arr = async(nowMonth) => {
   } catch(error) {
   console.log(error); 
   } 
+};
+
+// 시작시 페이지에 로딩되는 함수 - thisMonth() 오버라이딩
+window.onload = async function() {  
+  SessionCheck();
+  const getName = await axios.get('../php/LoadUser.php');
+  let userName = getName.data.username;
+  let name = getName.data.name;
+  let score = getName.data.score;
+  const nowMonth = ToDay.getMonth(); // 현재 Month
+
+  const [y, m] = getDate(new Date(ToDay.setMonth(nowMonth))); 
+
+  const lastDay = getDate(new Date(y, m, 0)).pop() * 1; 
+  const day = new Date([y, m, 1].join("-")).getDay() * 1;
+  const maxDay = Math.ceil((day + lastDay) / 7) * 7; 
+
+  arr(nowMonth, userName);
+
+  CheckedCnt(nowMonth, userName);
+
+  document.querySelector('.highlight-b').innerText = `${name}`;
+  document.querySelector('.highlight-a').innerText = `${name}`;
+
+  const dateInArr = ['dateIn1', 'dateIn2', 'dateIn3', 'dateIn4', 'dateIn5', 'dateIn6', 'dateIn7', 'dateIn8', 'dateIn9', 'dateIn10', 'dateIn11', 'dateIn12', 'dateIn13', 'dateIn14', 'dateIn15', 'dateIn16', 'dateIn17', 'dateIn18', 'dateIn19', 'dateIn20', 'dateIn21', 'dateIn22', 'dateIn23', 'dateIn24', 'dateIn25', 'dateIn26', 'dateIn27', 'dateIn28', 'dateIn29', 'dateIn30', 'dateIn31'];
+    const getName = await axios.get('../php/LoadUser.php');
+    let userName = getName.data.username;
+    const monthDay2 = await axios.post('../php/getMissionDay.php', {userName: userName, nowMonth: nowMonth+1});
+
+    let mission = document.querySelector(`.missions${monthDay2.data.length}`);
+    mission.style.display='none';
+    
+    const mission = document.querySelector(`.missions${monthDay2.data.length+1}`);
+    mission.style.display=''; 
 }
 
 // 값이 1로 하나씩 바뀔 때마다 스코어가 1씩 증가! 
-const changeScore = async() => {
+const changeScore = async(userName) => {
   try {
+    const getName = await axios.get('../php/LoadUser.php');
+    let userName = getName.data.username;
     let response = await axios.post('../php/changeScore.php', {userName: userName});
   
     if(response.data) {
@@ -138,11 +104,12 @@ const changeScore = async() => {
 const changeCB = async(i, nowMonth) => {
   try {
     let nowDay = i;
-    userName = 'eooomji';
+    const getName = await axios.get('../php/LoadUser.php');
+    let userName = getName.data.username;
     const response = await axios.post('../php/changeCheck.php', {nowMonth: nowMonth, nowDay: nowDay, userName: userName});
 
     if(response.data) {
-      // console.log(response.data);
+      console.log(`${nowMonth}월 ${nowDay}일의 미션 완료`);
     } else {
         alert("실패!");
     }

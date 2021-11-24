5bac8ec9f644be070cf5afbcb93c4d114028bcf4
XenicsKirk <92568796+XenicsKirk@users.noreply.github.com>
2021-11-25 02:32:20 +0900

    뻥튀기

--- php/getMissions.php
+++ php/getMissions.php
@@ -4,20 +4,26 @@
 
     $_POST = JSON_DECODE(file_get_contents("php://input"), true);
 
-    $nowMonth = $_POST["nowMonth"];
-    $userName = $_POST["userName"];
+    $nowMonth = 11;//$_POST["nowMonth"];
+    $userName = "username";//$_POST["userName"];
+    
+    // 현재 월의 마지막 날 구하는 구문
+    $time = "21-".$nowMonth."-21";
+    $LastDay = date("t", strtotime($time));
 
-    $sql = "SELECT missionName FROM misson_log WHERE username = '$userName' AND MONTH(date)=$nowMonth";
-
-    // $sql = "SELECT missionName FROM misson_log WHERE username = '$userName'";
+    $sql = "SELECT missionName, DAY(date) FROM misson_log WHERE username = '$userName' AND MONTH(date)=$nowMonth";
 
     $data = array();
 
     $res = $db->query($sql);
 
+    for($i = 1; $i <= $LastDay; $i++) {
+        $data[$i]["missionName"] = "0";
+    }
+    
     for($i = 0; $i < $res->num_rows; $i++) {
         $row = $res->fetch_array(MYSQLI_ASSOC);    
-        array_push($data, $row);
+        $data[$row["DAY(date)"]]["missionName"] = $row["missionName"];
     }
 
     if ($data != null) {

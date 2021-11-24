3c27549fdc06e5193be98066c3d6f73bc40d233d
XenicsKirk <92568796+XenicsKirk@users.noreply.github.com>
2021-11-24 13:39:21 +0900

    Add files via upload

---
+++ php/CheckID.php
@@ -0,0 +1,20 @@
+<?php
+    require_once("dbconfig.php");   // db 초기설정
+
+    $_POST = JSON_DECODE(file_get_contents("php://input"), true);
+
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+
+    $sqi = "SELECT `username` FROM `user` WHERE `name` = '$name' AND `email` = '$email'";
+    $res = $db->query($sqi);
+    $row = $res->fetch_array(MYSQLI_ASSOC);
+
+    if($row) {
+        echo JSON_ENCODE($row, JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK);
+    } else {
+        echo JSON_ENCODE(false, JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK);
+    }
+
+    mysqli_close($db);
+?>
---
+++ php/CheckPW.php
@@ -0,0 +1,21 @@
+<?php
+    require_once("dbconfig.php");   // db 초기설정
+
+    $_POST = JSON_DECODE(file_get_contents("php://input"), true);
+
+    $username = $_POST["username"];
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+
+    $sqi = "SELECT * FROM `user` WHERE `username` = '$username' AND `name` = '$name' AND `email` = '$email'";
+    $db->query($sqi);
+    $row = $res->fetch_array(MYSQLI_ASSOC);
+
+    if($row) {
+        echo JSON_ENCODE($row, JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK);
+    } else {
+        echo JSON_ENCODE(false, JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK);
+    }
+
+    mysqli_close($db);
+?>

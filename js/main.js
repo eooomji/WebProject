bd1719f1b50d51b4bcb84afa994b7c8b9655db8d
최승호 <75907683+etaF-LODi@users.noreply.github.com>
2021-11-25 12:30:03 +0900

    위치 수정

--- js/main.js
+++ js/main.js
@@ -10,15 +10,6 @@ window.onload = async function() {
   document.querySelector('.highlight-b').innerText = `${name}`;
   document.querySelector('.highlight-a').innerText = `${score}`;
 
-  let username = getName.data.username;
-  const getMission = await axios.post('../php/getMissions.php', {userName: userName, nowMonth: nowMonth+1});
-  const getMonth = await axios.post('../php/getMissionDay.php', {userName: userName, nowMonth: nowMonth+1});;
-
-  document.querySelector('.mission').innerText = `${getMission.data[getMonth.data.length].missionName}`;
-  
-  if (getMission.data){
-    document.getElementById("missioncheck").innerHTML = `<div class="oneDateSel missions${getMonth.data.length+1}"><input class="form-check-input" type="checkbox" name="${nowMonth+1}" onclick='getCheckedCnt(${getMonth.data.length+1}, ${nowMonth+1})'>${response.data[getMonth.data.length].missionName}</div>`;
-  }
   
   let massagecode = Math.floor(Math.random() * 3);
   switch(massagecode){
@@ -31,6 +22,16 @@ window.onload = async function() {
     case 2:
       document.getElementById("down").innerText = "오늘도 열심히 해봅시다!";
       break;
+  }
+  
+  let username = getName.data.username;
+  const getMission = await axios.post('../php/getMissions.php', {userName: userName, nowMonth: nowMonth+1});
+  const getMonth = await axios.post('../php/getMissionDay.php', {userName: userName, nowMonth: nowMonth+1});;
+
+  document.querySelector('.mission').innerText = `${getMission.data[getMonth.data.length].missionName}`;
+  
+  if (getMission.data){
+    document.getElementById("missioncheck").innerHTML = `<div class="oneDateSel missions${getMonth.data.length+1}"><input class="form-check-input" type="checkbox" name="${nowMonth+1}" onclick='getCheckedCnt(${getMonth.data.length+1}, ${nowMonth+1})'>${response.data[getMonth.data.length].missionName}</div>`;
   }
 }
 

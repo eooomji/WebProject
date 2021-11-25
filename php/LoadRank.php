0ae2a77d667d675b1453366501394cec37b61540
tmnt867 <jyj199803@gmail.com>
2021-11-25 15:05:43 +0900

    211125

--- php/LoadRank.php
+++ php/LoadRank.php
@@ -1,7 +1,7 @@
 <?php
 require_once("dbconfig.php"); 
 
-$sql = "SELECT `name`, `score` FROM `user` ORDER BY `score` DESC";
+$sql = "SELECT `name`, `score` FROM `user` ORDER BY `score` DESC"; // 1위부터 전부 출력. 별도 조건 설정 시 아래의 StartRank 도 변경
 $res = $db->query($sql); 
 
 if($res) {
